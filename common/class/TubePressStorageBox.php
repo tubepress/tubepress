219aeaabfb42e081cb3df3f63a5751e82c41ed18
Eric Hough <houghdev@gmail.com>
2007-08-05 21:51:34 +0000

    Adding left over stuff...
    git-svn-id: http://tubepress.googlecode.com/svn/trunk@35 4dae44e4-133b-0410-9315-df2bff685ba4

---
+++ common/class/TubePressStorageBox.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * TubePressStorageBox.php
+ * 
+ * Copyright (C) 2007 Eric D. Hough (http://ehough.com)
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+/**
+ * This is meant to be an abstract class, though PHP 4 doesn't support
+ * them :(. The idea here is that each implementation (WordPress, MoveableType)
+ * extends this class and passes it around as the class that holds all 
+ * of the users options. It's essentially just an array of TubePressOptions 
+ * with some extra methods related to metadata on those options.
+*/
+class TubePressStorageBox
+{
+    /* this is our array of items */
+    var $options, $players, $modes;
+    
+    /**
+     * Default options
+     */
+    function TubePressStorageBox()
+    {
+		$this->options = new TubePressOptionsPackage();
+		$this->players = new TubePressPlayerPackage();
+		$this->modes = new TubePressModesPackage();
+    }
+    
+    function checkValidity()
+    {
+        $packages = array($this->options, $this->players, $this->modes);
+        foreach($packages as $package) {
+            if (!is_a($package, "TubePressDataPackage")) {
+                return PEAR::raiseError("Your options are old!");
+            }
+            $test = $package->checkValidity();
+            
+            if (PEAR::isError(($test))) {
+                return $test;
+            }
+        }
+        return NULL;
+    }
+}
+?>
