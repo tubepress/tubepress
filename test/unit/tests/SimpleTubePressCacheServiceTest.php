f44d83c397277d9ad9d2b306f1ce896de49d2ee4
Eric Hough <houghdev@gmail.com>
2008-12-08 06:34:55 +0000

    SimpleTubePressCacheService is at 100% coverage
    git-svn-id: http://tubepress.googlecode.com/svn/trunk@246 4dae44e4-133b-0410-9315-df2bff685ba4

--- test/unit/tests/SimpleTubePressCacheServiceTest.php
+++ test/unit/tests/SimpleTubePressCacheServiceTest.php
@@ -1,16 +1,35 @@
 <?php
 class SimpleTubePressCacheServiceTest extends PHPUnit_Framework_TestCase {
-    
+
 	private $_sut;
-	
+
 	function setUp()
 	{
 		$this->_sut = new SimpleTubePressCacheService();
 	}
-	
-	function testGet()
+
+	function testSetGet()
 	{
-		
+		$key = $this->_randomString();
+		$data = $this->_randomString();
+		$this->_sut->save($key, $data);
+		$this->assertTrue($this->_sut->has($key));
+		$this->assertEquals($data, $this->_sut->get($key));
+	}
+
+	private function _randomString() {
+		$chars = "abcdefghijkmnopqrstuvwxyz023456789";
+		srand((double)microtime()*1000000);
+		$i = 0;
+		$pass = '' ;
+
+		while ($i <= 7) {
+			$num = rand() % 33;
+			$tmp = substr($chars, $num, 1);
+			$pass = $pass . $tmp;
+			$i++;
+		}
+		return $pass;
 	}
 }
 ?>
