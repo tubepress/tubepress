d8a88dbbe8ea3af365fc759fc595fc7010ff1955
Eric Hough <houghdev@gmail.com>
2008-12-08 00:51:51 +0000

    Re-organizing tests
    git-svn-id: http://tubepress.googlecode.com/svn/trunk@243 4dae44e4-133b-0410-9315-df2bff685ba4

--- test/TubePressTestSuite.php
+++ test/TubePressTestSuite.php
@@ -3,12 +3,18 @@
 require_once '/Applications/MAMP/bin/php5/lib/php/PHPUnit/Framework.php';
 include dirname(__FILE__) . "/../tubepress_classloader.php";
 
-require_once "common/class/TubePressVideoTest.php";
-require_once "common/class/util/TubePressStringUtilsTest.php";
-require_once "common/class/util/TubePressShortcodeTest.php";
-require_once "common/class/options/SimpleTubePressOptionsManagerTest.php";
-require_once "common/class/options/TubePressValidatorTest.php";
-require_once "common/class/options/category/TubePressAdvancedOptionsTest.php";
+$testNames = array(
+	"TubePressVideoTest",
+	"TubePressStringUtilsTest",
+	"TubePressShortcodeTest",
+	"SimpleTubePressOptionsManagerTest",
+	"TubePressValidatorTest",
+	"TubePressAdvancedOptionsTest"
+);
+
+foreach ($testNames as $test) {
+	require_once "tests/" . $test . ".php";
+}
 
 function __($key) {
 	return $key;
@@ -18,13 +24,11 @@ class TubePressTestSuite
 {
 	public static function suite()
 	{
+		global $testNames;
 		$suite = new PHPUnit_Framework_TestSuite("TubePress Tests");
-		$suite->addTestSuite("TubePressVideoTest");
-		$suite->addTestSuite("TubePressStringUtilsTest");
-		$suite->addTestSuite("TubePressShortcodeTest");
-		$suite->addTestSuite("SimpleTubePressOptionsManagerTest");
-		$suite->addTestSuite("TubePressValidatorTest");
-		$suite->addTestSuite("TubePressAdvancedOptionsTest");
+		foreach ($testNames as $test) {
+			$suite->addTestSuite($test);
+		}
 		return $suite;
 	}
 }
