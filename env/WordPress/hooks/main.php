7442e7b248912715bc26294ad7e24c21824f70db
Eric Hough <houghdev@gmail.com>
2009-01-22 05:30:55 +0000

    Commenting main.php for WordPress
    git-svn-id: http://tubepress.googlecode.com/svn/trunk@343 4dae44e4-133b-0410-9315-df2bff685ba4

--- env/WordPress/hooks/main.php
+++ env/WordPress/hooks/main.php
@@ -21,11 +21,20 @@
 
 if (version_compare(PHP_VERSION, '5.0.0', '>=')
         && !function_exists("tubepress_content_filter")) {
-        	
+    
+    /* set the tubepress_base_url global */
+    $tubepress_base_url = get_settings('siteurl') . "/wp-content/plugins/tubepress";        
+            
+    /* register the plugin's message bundles */
 	load_plugin_textdomain('tubepress', 'wp-content/plugins/tubepress/i18n');
+	
+	/* load up the rest of the WordPress specific code */
     include dirname(__FILE__) . '/../functions/main.php';
 
+    /* add a filter for all post/page content */
 	add_filter('the_content', 'tubepress_content_filter');
+	
+	/* add a filter so we can add our CSS/JS to the head */
 	add_action('wp_head',     'tubepress_head_filter');
 }
 
