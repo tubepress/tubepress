5fa9d5734d79b950752a2abeb4452eeb8158254c
Eric Hough <houghdev@gmail.com>
2009-03-24 21:52:13 +0000

    Adding classloading stuff for Gallery.class.php
    git-svn-id: http://tubepress.googlecode.com/svn/trunk@461 4dae44e4-133b-0410-9315-df2bff685ba4

--- classes/org/tubepress/gallery/Gallery.class.php
+++ classes/org/tubepress/gallery/Gallery.class.php
@@ -19,6 +19,10 @@
  *
  */
 
+function_exists('tubepress_load_classes')
+    || require(dirname(__FILE__) . '/../../../tubepress_classloader.php');
+tubepress_load_classes(array('org_tubepress_gallery_AbstractGallery'));
+
 /**
  * Regular TubePress gallery
  */
