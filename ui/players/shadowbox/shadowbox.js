28d7725e7e74ecee65ba07a5c1459d9b1ea1ea06
Eric Hough <houghdev@gmail.com>
2009-05-04 05:39:42 +0000

    Shadowbox generally works again
    git-svn-id: http://tubepress.googlecode.com/svn/trunk@501 4dae44e4-133b-0410-9315-df2bff685ba4

--- ui/players/shadowbox/shadowbox.js
+++ ui/players/shadowbox/shadowbox.js
@@ -1,30 +1,62 @@
 function tubepress_shadowbox_player_init(baseUrl) {
-	var shadowboxBase = baseUrl + '/ui/players/shadowbox/';
-	jQuery.include(shadowboxBase + 'lib/skin/classic/skin.css');
-	_tubepress_load_shadowbox_base(shadowboxBase);
+	var base = baseUrl + '/ui/players/shadowbox/';
+	//jQuery.include(base + 'lib/shadowbox.css', function(){
+		_tubepress_shadowbox_player_shadowboxjs(base);
+	//});
+}
+
+function _tubepress_shadowbox_player_shadowboxjs(base)  {
+	_tubepress_get_wait_call(base + 'lib/shadowbox.js',
+			function() { return typeof Shadowbox != 'undefined'; },
+			function() { _tubepress_shadowbox_player_shadowboxlang(base); }
+		);
+}
+
+function _tubepress_shadowbox_player_shadowboxlang(base) {
+	_tubepress_get_wait_call(base + 'lib/languages/shadowbox-en.js',
+			function() { return typeof Shadowbox.lang != 'undefined'; },
+			function() { _tubepress_shadowbox_player_shadowboxplayer(base); }
+		);
+}
+
+function _tubepress_shadowbox_player_shadowboxplayer(base) {
+	_tubepress_get_wait_call(base + 'lib/players/shadowbox-html.js',
+			function() { return typeof Shadowbox.html != 'undefined'; },
+			function() { _tubepress_shadowbox_player_shadowboxadapter(base); }
+		);
+}
+
+function _tubepress_shadowbox_player_shadowboxadapter(base) {
+	_tubepress_get_wait_call(base + 'lib/adapters/shadowbox-jquery.js',
+			function() { return typeof Shadowbox.lib != 'undefined'; },
+			function() { _tubepress_init_shadowbox(base); }
+		);
 }
 
 function tubepress_shadowbox_player(galleryId, videoId) {
-    var flashObj = jQuery("#tubepress_embedded_object_" + galleryId + " > object");
+	var wrapperId = "#tubepress_embedded_object_" + galleryId,
+		wrapper = jQuery(wrapperId),
+		obj = jQuery(wrapperId + " > object"),
+		params = obj.children("param"),
+		videoTitleAnchor = jQuery("#tubepress_title_" + videoId + "_" + galleryId);
 	Shadowbox.open({
 	    player:       'html',
-	    title:        jQuery("#tubepress_image_" + videoId + "_" + galleryId + " > img").attr("alt"),
-	    content:      jQuery("#tubepress_embedded_object_" + galleryId).html(),
-	    height:       flashObj.css("height"),
-	    width:        flashObj.css("width")
+	    title:        videoTitleAnchor.html(),
+	    content:      tubepress_deep_construct_object(wrapper, params),
+	    height:       obj.css("height"),
+	    width:        obj.css("width")
 	});
 }
 
-function _tubepress_load_shadowbox_skin(base) {
-	_tubepress_get_wait_call(base + 'lib/skin/classic/skin.js',
-		function() { return typeof Shadowbox.SKIN != 'undefined'; },
-		function() { Shadowbox.init({skipSetup: true}); }
-	);
+function _tubepress_init_shadowbox(base) {
+	Shadowbox.path = base + 'lib/';
+	Shadowbox.init({
+		initialHeight : 160,
+		initialWidth: 320,
+		skipSetup: true, 
+		players: ["html"],
+		useSizzle: false
+	});
+	Shadowbox.load();
 }
 
-function _tubepress_load_shadowbox_base(base) {
-	_tubepress_get_wait_call(base + 'lib/shadowbox-2.0.js',
-		function() { return typeof Shadowbox.init == 'function'; },
-		function() { _tubepress_load_shadowbox_skin(base); }
-	);
-}
