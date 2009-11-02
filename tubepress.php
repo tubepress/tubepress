1b1c2a218cbc3054c16cca24cc8023993fa49d9f
Eric Hough <houghdev@gmail.com>
2009-11-02 03:37:13 +0000

    Version bump
    git-svn-id: http://tubepress.googlecode.com/svn/trunk@659 4dae44e4-133b-0410-9315-df2bff685ba4

--- tubepress.php
+++ tubepress.php
@@ -4,7 +4,7 @@ Plugin Name: TubePress
 Plugin URI: http://tubepress.org
 Description: Displays gorgeous YouTube galleries in your posts, pages, and/or sidebar. Upgrade to <a href="http://tubepress.org/features/">TubePress Pro</a> for more features!
 Author: Eric D. Hough
-Version: 1.8.6-svn
+Version: 1.8.7-svn
 Author URI: http://ehough.com
 
 Copyright 2006, 2007, 2008, 2009 Eric D. Hough (http://ehough.com)
