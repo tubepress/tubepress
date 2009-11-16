280e1738f6fade1dda8d0567d78052d743f02868
Eric Hough <houghdev@gmail.com>
2009-11-16 00:50:02 +0000

    Adding copyright notice to template files
    git-svn-id: http://tubepress.googlecode.com/svn/trunk@668 4dae44e4-133b-0410-9315-df2bff685ba4

--- ui/embedded/longtail/html_templates/object.tpl.php
+++ ui/embedded/longtail/html_templates/object.tpl.php
@@ -1,3 +1,24 @@
+<?php 
+/**
+ * Copyright 2006, 2007, 2008, 2009 Eric D. Hough (http://ehough.com)
+ * 
+ * This file is part of TubePress (http://tubepress.org)
+ * 
+ * TubePress is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * TubePress is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with TubePress.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+?>
 <object type="application/x-shockwave-flash" data="<?php echo ${org_tubepress_template_Template::TUBEPRESS_BASE_URL}; ?>/ui/embedded/longtail/lib/player.swf" style="width: <?php echo ${org_tubepress_template_Template::EMBEDDED_WIDTH}; ?>px; height: <?php echo ${org_tubepress_template_Template::EMBEDDED_HEIGHT}; ?>px" >
   
         <param name="AllowScriptAccess" value="never" />
