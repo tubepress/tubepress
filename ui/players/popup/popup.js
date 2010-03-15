e55f09c959486df72736f1abe5dfea452d50aebb
Eric Hough <houghdev@gmail.com>
2010-03-15 22:04:51 +0000

    Fix for issue 160
    git-svn-id: http://tubepress.googlecode.com/svn/trunk@833 4dae44e4-133b-0410-9315-df2bff685ba4

--- ui/players/popup/popup.js
+++ ui/players/popup/popup.js
@@ -3,14 +3,17 @@ function tubepress_popup_player(galleryId, videoId) {
 		wrapper = jQuery(wrapperId),
 		obj = jQuery(wrapperId + " > object"),
 		params = obj.children("param"),
-		win = window.open('', '', 
-    		'location=0,directories=0,menubar=0,scrollbars=0,status=0,toolbar=0,width=' + obj.css("width") + ',height=' + obj.css("height")),
-    	preamble = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">\n<html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html;charset=utf-8" /><title>' + 
-    		jQuery("#tubepress_title_" + videoId + "_" + galleryId).html() + '</title></head><body style="margin: 0pt; background-color: black;">',
-    	newHtml = TubePress.deepConstructObject(wrapper, params);
-    
-    win.document.write(preamble + newHtml + '</body></html>');
-    win.document.close() ;
+		wWidth = obj.css("width"),
+		wHeight = obj.css("height"),
+		top =  (screen.height/2)-(wHeight/2),
+		left = (screen.width/2)-(wWidth/2),
+		win = window.open('', '', 'location=0,directories=0,menubar=0,scrollbars=0,status=0,toolbar=0,width=' + wWidth + ',height=' + wHeight + ',top=' + top + ',left=' + left),
+		preamble = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">\n<html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html;charset=utf-8" /><title>' + 
+		jQuery("#tubepress_title_" + videoId + "_" + galleryId).html() + '</title></head><body style="margin: 0pt; background-color: black;">',
+		newHtml = TubePress.deepConstructObject(wrapper, params);
+
+	win.document.write(preamble + newHtml + '</body></html>');
+	win.document.close() ;
 }
 
 function tubepress_popup_player_init(baseUrl) {  }
