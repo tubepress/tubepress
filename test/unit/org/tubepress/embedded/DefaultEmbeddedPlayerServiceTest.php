082bd10041ab852920d47ba2e068f7fa9e80cf37
Eric Hough <houghdev@gmail.com>
2010-04-14 22:41:58 +0000

    More tests for embedded player service
    git-svn-id: http://tubepress.googlecode.com/svn/trunk@881 4dae44e4-133b-0410-9315-df2bff685ba4

---
+++ test/unit/org/tubepress/embedded/DefaultEmbeddedPlayerServiceTest.php
@@ -0,0 +1,70 @@
+<?php
+
+require_once dirname(__FILE__) . '/../../../../../classes/org/tubepress/embedded/impl/DefaultEmbeddedPlayerService.class.php';
+
+class org_tubepress_embedded_impl_DefaultEmbeddedPlayerServiceTest extends PHPUnit_Framework_TestCase {
+    
+    private $_sut;
+    private $_vimeo;
+    private $_youtube;
+    private $_tpom;
+
+	function setUp()
+	{
+		$this->_sut = new org_tubepress_embedded_impl_DefaultEmbeddedPlayerService();
+		$this->createMocks();
+		$this->applyMocks();
+	}
+
+	function testFallback()
+	{
+		$this->_tpom->expects($this->once())
+		     ->method('calculateCurrentVideoProvider')
+		     ->will($this->returnValue('something strange'));
+		$this->_youtube->expects($this->once())
+		     ->method('toString')
+		     ->will($this->returnValue('youtube'));
+		$result = $this->_sut->toString('somevideo');
+		$this->assertEquals('youtube', $result);
+	}
+
+	function testYouTube()
+	{
+		$this->_tpom->expects($this->once())
+		     ->method('calculateCurrentVideoProvider')
+		     ->will($this->returnValue(org_tubepress_video_feed_provider_Provider::YOUTUBE));
+		$this->_youtube->expects($this->once())
+		     ->method('toString')
+		     ->will($this->returnValue('youtube'));
+		$result = $this->_sut->toString('somevideo');
+		$this->assertEquals('youtube', $result);
+	}
+	
+	function testVimeo()
+	{
+		$this->_tpom->expects($this->once())
+		     ->method('calculateCurrentVideoProvider')
+		     ->will($this->returnValue(org_tubepress_video_feed_provider_Provider::VIMEO));
+		$this->_vimeo->expects($this->once())
+		     ->method('toString')
+		     ->will($this->returnValue('vimeo'));
+		$result = $this->_sut->toString('somevideo');
+		$this->assertEquals('vimeo', $result);
+	}
+	
+	function applyMocks()
+	{
+		$this->_sut->setVimeoEmbeddedPlayerService($this->_vimeo);
+		$this->_sut->setYouTubeEmbeddedPlayerService($this->_youtube);
+		$this->_sut->setOptionsManager($this->_tpom);	
+	}
+	
+	function createMocks()
+	{
+		$this->_vimeo = $this->getMock('org_tubepress_embedded_impl_VimeoEmbeddedPlayerService');
+		$this->_youtube = $this->getMock('org_tubepress_embedded_impl_YouTubeEmbeddedPlayerService');
+		$this->_tpom = $this->getMock('org_tubepress_options_manager_OptionsManager');
+	}
+	
+}
+?>
--- test/unit/org/tubepress/embedded/EmbeddedTests.php
+++ test/unit/org/tubepress/embedded/EmbeddedTests.php
@@ -2,6 +2,8 @@
 require_once '/Applications/MAMP/bin/php5/lib/php/PHPUnit/Framework.php';
 require_once 'JwFlvEmbeddedPlayerServiceTest.php';
 require_once 'YouTubeEmbeddedPlayerServiceTest.php';
+require_once 'VimeoEmbeddedPlayerServiceTest.php';
+require_once 'DefaultEmbeddedPlayerServiceTest.php';
 
 class EmbeddedTests
 {
@@ -10,6 +12,8 @@ class EmbeddedTests
 		$suite = new PHPUnit_Framework_TestSuite("TubePress Embedded Player Tests");
 		$suite->addTestSuite('org_tubepress_embedded_impl_YouTubeEmbeddedPlayerServiceTest');
 		$suite->addTestSuite('org_tubepress_embedded_impl_JwFlvEmbeddedPlayerServiceTest');
+		$suite->addTestSuite('org_tubepress_embedded_impl_VimeoEmbeddedPlayerServiceTest');
+		$suite->addTestSuite('org_tubepress_embedded_impl_DefaultEmbeddedPlayerServiceTest');
 		return $suite;
 	}
 }
