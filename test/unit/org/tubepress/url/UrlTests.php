c7aed4c72d6a316da2cb41133ed92f2f436fbd1c
Eric Hough <houghdev@gmail.com>
2010-04-15 01:10:54 +0000

    Adding more vimeo URL builder tests
    git-svn-id: http://tubepress.googlecode.com/svn/trunk@886 4dae44e4-133b-0410-9315-df2bff685ba4

--- test/unit/org/tubepress/url/UrlTests.php
+++ test/unit/org/tubepress/url/UrlTests.php
@@ -1,6 +1,7 @@
 <?php
 require_once '/Applications/MAMP/bin/php5/lib/php/PHPUnit/Framework.php';
 require_once 'YouTubeUrlBuilderTest.php';
+require_once 'VimeoUrlBuilderTest.php';
 
 class UrlTests
 {
@@ -8,6 +9,7 @@ class UrlTests
 	{
 		$suite = new PHPUnit_Framework_TestSuite("TubePress URL Tests");
 		$suite->addTestSuite('org_tubepress_url_YouTubeUrlBuilderTest');
+		$suite->addTestSuite('org_tubepress_url_VimeoUrlBuilderTest');
 		return $suite;
 	}
 }
