cb883c1acc915afd0ae0e1b1738fb20a4489bf3a
Eric Hough <houghdev@gmail.com>
2010-07-15 22:00:16 +0000

    Code cleanup.
    git-svn-id: http://tubepress.googlecode.com/svn/trunk@958 4dae44e4-133b-0410-9315-df2bff685ba4

--- classes/org/tubepress/options/validation/SimpleInputValidationService.class.php
+++ classes/org/tubepress/options/validation/SimpleInputValidationService.class.php
@@ -20,12 +20,15 @@
  */
 
 function_exists('tubepress_load_classes')
-    || require(dirname(__FILE__) . '/../../../../tubepress_classloader.php');
+    || require dirname(__FILE__) . '/../../../../tubepress_classloader.php';
 tubepress_load_classes(array('org_tubepress_options_validation_InputValidationService',
     'org_tubepress_options_category_Display',
     'org_tubepress_message_MessageService',
     'org_tubepress_options_reference_OptionsReference',
-    'org_tubepress_log_Log'));
+    'org_tubepress_log_Log',
+    'org_tubepress_options_Type',
+    'org_tubepress_options_category_Gallery',
+    'org_tubepress_options_category_Uploads'));
 
 /**
  * Default implementation of org_tubepress_options_validation_InputValidationService
@@ -36,14 +39,23 @@ class org_tubepress_options_validation_SimpleInputValidationService implements o
     private $_logPrefix;
     private $_messageService;
     private $_optionsReference;
-    
+
+    /**
+     * Constructor.
+     */
     public function __construct()
     {
         $this->_logPrefix = 'Input Validation Service';
     }
-    
+
     /**
-     * @see org_tubepress_options_validation_InputValidationService::validate($optionName, $candidate)
+     * Validates an option value. Will throw an Exception if validation
+     * fails.
+     *
+     * @param string       $optionName The option name
+     * @param unknown_type $candidate  The candidate option value
+     *
+     * @return void
     */
     public function validate($optionName, $candidate)
     {
@@ -52,7 +64,7 @@ class org_tubepress_options_validation_SimpleInputValidationService implements o
 
         /* is the value given of the right type? */
         $this->_checkType($optionName, $candidate);
-        
+
         /* perform any custom validation */
         $this->_customValidation($optionName, $candidate);
     }
@@ -60,113 +72,114 @@ class org_tubepress_options_validation_SimpleInputValidationService implements o
     /**
      * Performs "one off" validation for options
      *
-     * @param $optionName string The name of the option to validate
-     * @param $candidate unknown The value of the option to validate
+     * @param string  $optionName The name of the option to validate
+     * @param unknown $candidate  The value of the option to validate
      *
      * @return void
      */
     private function _customValidation($optionName, $candidate)
     {
         switch ($optionName) {
-        
-            /* YouTube limits us to 50 per page */
-            case org_tubepress_options_category_Display::RESULTS_PER_PAGE:
-                $this->_checkIntegerRange(org_tubepress_options_category_Display::RESULTS_PER_PAGE, $candidate, 1, 50);
-                break;
-            
-            case org_tubepress_options_category_Gallery::TEMPLATE:
-                if (strpos($candidate, '..') !== FALSE) {
-                    throw new Exception($this->_messageService->_('validation-no-dots-in-path'));
-                }
-                break;
-            
-            case org_tubepress_options_category_Uploads::FFMPEG_BINARY_LOCATION:
-                if (!is_executable($candidate)) {
-                    throw new Exception(sprintf($this->_messageService->_('validation-ffmpeg-not-executable'), $candidate));
-                }
-                break;
-            case org_tubepress_options_category_Uploads::VIDEO_UPLOADS_BASE_DIRECTORY:
-                if ($candidate != '' && !is_dir($candidate)) {
-                	throw new Exception(sprintf($this->_messageService->_('validation-directory-not-directory'), $candidate));	
-                }
-                if ($candidate != '' && !is_readable($candidate)) {
-                	throw new Exception(sprintf($this->_messageService->_('validation-directory-not-readable'), $candidate));	
-                }
-                break;
-            case org_tubepress_options_category_Gallery::DIRECTORY_VALUE:
-                if (strpos($candidate, '..') !== FALSE) {
-                    throw new Exception($this->_messageService->_('validation-no-dots-in-path'));
-                }
-                break;
+
+        /* YouTube limits us to 50 per page */
+        case org_tubepress_options_category_Display::RESULTS_PER_PAGE:
+            $this->_checkIntegerRange(org_tubepress_options_category_Display::RESULTS_PER_PAGE, $candidate, 1, 50);
+            break;
+
+        case org_tubepress_options_category_Gallery::TEMPLATE:
+            if (strpos($candidate, '..') !== false) {
+                throw new Exception($this->_messageService->_('validation-no-dots-in-path'));
+            }
+            break;
+
+        case org_tubepress_options_category_Uploads::FFMPEG_BINARY_LOCATION:
+            if (!is_executable($candidate)) {
+                throw new Exception(sprintf($this->_messageService->_('validation-ffmpeg-not-executable'), $candidate));
+            }
+            break;
+
+        case org_tubepress_options_category_Uploads::VIDEO_UPLOADS_BASE_DIRECTORY:
+            if ($candidate != '' && !is_dir($candidate)) {
+                throw new Exception(sprintf($this->_messageService->_('validation-directory-not-directory'), $candidate));
+            }
+            if ($candidate != '' && !is_readable($candidate)) {
+                throw new Exception(sprintf($this->_messageService->_('validation-directory-not-readable'), $candidate));
+            }
+            break;
+
+        case org_tubepress_options_category_Gallery::DIRECTORY_VALUE:
+            if (strpos($candidate, '..') !== false) {
+                throw new Exception($this->_messageService->_('validation-no-dots-in-path'));
+            }
+            break;
         }
     }
-    
+
     /**
      * Verifies if the given option name exists
      *
-     * @param $optionName string The option name to check
+     * @param string $optionName The option name to check
      *
      * @return void
      */
     private function _checkExistence($optionName)
     {
-        if ($this->_optionsReference->isOptionName($optionName) === FALSE) {
+        if ($this->_optionsReference->isOptionName($optionName) === false) {
             throw new Exception(sprintf($this->_messageService->_('validation-no-such-option'), $optionName));
         }
     }
-    
+
     /**
      * Checks if the option value has the right type
      *
-     * @param $optionName string The name of the option to validate
-     * @param $candidate unknown The value of the option to validate
+     * @param string  $optionName The name of the option to validate
+     * @param unknown $candidate  The value of the option to validate
      *
      * @return void
      */
     private function _checkType($optionName, $candidate)
     {
         $type = $this->_optionsReference->getType($optionName);
-        
+
         switch ($type) {
-            case org_tubepress_options_Type::TEXT:
-            case org_tubepress_options_Type::YT_USER:
-            case org_tubepress_options_Type::PLAYLIST:
-                if (!is_string($candidate)) {
-                    throw new Exception(sprintf($this->_messageService->_('validation-text'), 
-                        $optionName, $candidate));
-                }
-                break;
-            case org_tubepress_options_Type::BOOL:
-                if (strcasecmp((string)$candidate, '1') !== 0 && strcasecmp((string)$candidate, '') !== 0) {
-                    throw new Exception(sprintf($this->_messageService->_('validation-bool'), 
-                        $optionName, $candidate));
-                }
-                break;
-            case org_tubepress_options_Type::INTEGRAL:
-                if (intval($candidate) == 0 && $optionName != org_tubepress_options_category_Display::DESC_LIMIT) {
-                    throw new Exception(sprintf($this->_messageService->_('validation-int-type'), 
-                        $optionName, $candidate));
-                }
-                break;
-            case org_tubepress_options_Type::MODE:
-            case org_tubepress_options_Type::ORDER:
-            case org_tubepress_options_Type::PLAYER:
-            case org_tubepress_options_Type::PLAYER_IMPL:
-            case org_tubepress_options_Type::SAFE_SEARCH:
-            case org_tubepress_options_Type::TIME_FRAME:
-                $validValues = $this->_optionsReference->getValidEnumValues($type);
-                if (in_array((string)$candidate, $validValues) !== TRUE) {
-                    throw new Exception(sprintf($this->_messageService->_('validation-enum'),
-                        $optionName, implode(', ', $validValues), $candidate
-                    ));
-                }
-                break;
-            case org_tubepress_options_Type::COLOR:
-                //implement me please
-                break;
+        case org_tubepress_options_Type::TEXT:
+        case org_tubepress_options_Type::YT_USER:
+        case org_tubepress_options_Type::PLAYLIST:
+            if (!is_string($candidate)) {
+                throw new Exception(sprintf($this->_messageService->_('validation-text'), $optionName, $candidate));
+            }
+            break;
+
+        case org_tubepress_options_Type::BOOL:
+            if (strcasecmp((string)$candidate, '1') !== 0 && strcasecmp((string)$candidate, '') !== 0) {
+                throw new Exception(sprintf($this->_messageService->_('validation-bool'), $optionName, $candidate));
+            }
+            break;
+
+        case org_tubepress_options_Type::INTEGRAL:
+            if (intval($candidate) == 0 && $optionName != org_tubepress_options_category_Display::DESC_LIMIT) {
+                throw new Exception(sprintf($this->_messageService->_('validation-int-type'), $optionName, $candidate));
+            }
+            break;
+
+        case org_tubepress_options_Type::MODE:
+        case org_tubepress_options_Type::ORDER:
+        case org_tubepress_options_Type::PLAYER:
+        case org_tubepress_options_Type::PLAYER_IMPL:
+        case org_tubepress_options_Type::SAFE_SEARCH:
+        case org_tubepress_options_Type::TIME_FRAME:
+            $validValues = $this->_optionsReference->getValidEnumValues($type);
+            if (in_array((string)$candidate, $validValues) !== true) {
+                throw new Exception(sprintf($this->_messageService->_('validation-enum'), $optionName, implode(', ', $validValues), $candidate));
+            }
+            break;
+
+        case org_tubepress_options_Type::COLOR:
+            //implement me please
+            break;
         }
     }
-    
+
     /**
      * Checks the range of integral values
      *
@@ -182,11 +195,20 @@ class org_tubepress_options_validation_SimpleInputValidationService implements o
     private function _checkIntegerRange($name, $candidate, $min, $max)
     {
         if ($candidate < $min || $candidate > $max) {
-            throw new Exception(sprintf($this->_messageService->_('validation-int-range'), 
-                $name, $min, $max, $candidate));
+            throw new Exception(sprintf($this->_messageService->_('validation-int-range'), $name, $min, $max, $candidate));
         }
     }
-   
-    public function setMessageService(org_tubepress_message_MessageService $messageService) { $this->_messageService = $messageService; }
+
+    /**
+     * Set the message service.
+     *
+     * @param org_tubepress_message_MessageService $messageService The message service.
+     *
+     * @return void
+     */
+    public function setMessageService(org_tubepress_message_MessageService $messageService)
+    {
+        $this->_messageService = $messageService;
+    }
     public function setOptionsReference(org_tubepress_options_reference_OptionsReference $reference) { $this->_optionsReference = $reference; }
 }
