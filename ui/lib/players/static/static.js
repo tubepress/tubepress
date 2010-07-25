9107bc2bc2bebe03791d29930fa8e312adf17d7c
Eric Hough <houghdev@gmail.com>
2010-07-25 01:15:06 +0000

    Static player now works correctly when no pagination is set
    git-svn-id: http://tubepress.googlecode.com/svn/trunk@1025 4dae44e4-133b-0410-9315-df2bff685ba4

--- ui/lib/players/static/static.js
+++ ui/lib/players/static/static.js
@@ -7,7 +7,7 @@
  * Shrink your JS: http://developer.yahoo.com/yui/compressor/
  */
 function tubepress_static_player_init(baseUrl) {
-	TubePressUtils.getWaitCall(baseUrl + '/ui/players/static/lib/jQuery.query.js',
+	TubePressUtils.getWaitCall(baseUrl + '/ui/lib/players/static/lib/jQuery.query.js',
 		_tubepress_static_player_readyTest,
 		_tubepress_static_player_init);
 	jQuery(document).bind('tubepressNewThumbnailsLoaded', function (x) {
@@ -21,15 +21,22 @@ function _tubepress_static_player_readyTest() {
 
 function _tubepress_static_player_init() {
 	jQuery("a[id^='tubepress_']").each(function() {
-		var dis 	= jQuery(this),
-		    rel_split 	= dis.attr('rel').split('_');
+		var dis       = jQuery(this),
+		    rel_split = dis.attr('rel').split('_');
 		
 		if (TubePress.getPlayerNameFromRelSplit(rel_split) != 'static') {
 			return;
 		}
+		
 		var newId 	= TubePress.getVideoIdFromIdAttr(dis.attr("id")),
-			page	= jQuery(dis).parents("div.tubepress_thumbnail_area:first > div.pagination:first > span.current").html(),
-		    	newUrl 	= jQuery.query.set('tubepress_video', newId).set('tubepress_page', page).toString();
+		    page = 1,
+		    paginationSelector = "div.tubepress_thumbnail_area:first > div.pagination:first > span.current";
+		
+		if (jQuery(dis).parents(paginationSelector).length > 0) {
+			page = jQuery(dis).parents(paginationSelector).html()
+		}
+		
+		var newUrl 	= jQuery.query.set('tubepress_video', newId).set('tubepress_page', page).toString();
 		dis.attr('href', newUrl);
 		dis.unbind('click', TubePress.clickListener);
 	});
