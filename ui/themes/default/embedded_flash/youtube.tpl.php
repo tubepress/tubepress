04cf74724c695ff2badee4326e6a19d1e11ce003
Eric Hough <houghdev@gmail.com>
2010-07-30 21:34:14 +0000

    Accidentally checked in debugging stuff
    git-svn-id: http://tubepress.googlecode.com/svn/trunk@1080 4dae44e4-133b-0410-9315-df2bff685ba4

--- ui/themes/default/embedded_flash/youtube.tpl.php
+++ ui/themes/default/embedded_flash/youtube.tpl.php
@@ -19,7 +19,7 @@
  *
  */
 ?>
-<object type="application/x-shockwave-flash" data="<?php echo ${org_tubepress_template_Template::EMBEDDED_DATA_URL}; ?>" style="width:100px;height:100px">
+<object type="application/x-shockwave-flash" data="<?php echo ${org_tubepress_template_Template::EMBEDDED_DATA_URL}; ?>" style="width: <?php echo ${org_tubepress_template_Template::EMBEDDED_WIDTH}; ?>px; height: <?php echo ${org_tubepress_template_Template::EMBEDDED_HEIGHT}; ?>px">
   
         <param name="wmode" value="transparent" />
         <param name="movie" value="<?php echo ${org_tubepress_template_Template::EMBEDDED_DATA_URL}; ?>" />
