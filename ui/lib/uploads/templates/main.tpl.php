889ef97190c9ea1f405832b3241f1cd2b6fdfce3
Eric Hough <houghdev@gmail.com>
2010-08-09 22:38:58 +0000

    More work on uploads admin page
    git-svn-id: http://tubepress.googlecode.com/svn/trunk@1098 4dae44e4-133b-0410-9315-df2bff685ba4

--- ui/lib/uploads/templates/main.tpl.php
+++ ui/lib/uploads/templates/main.tpl.php
@@ -24,60 +24,63 @@
 
 $idArray = array();
 ?>
-<p>
-Your video uploads base directory is <code><?php echo ${org_tubepress_uploads_admin_AdminPageHandler::PATH_TO_VIDEO_UPLOADS}; ?></code>
-</p>
+<p>Your video uploads base directory is <code><?php echo ${org_tubepress_uploads_admin_AdminPageHandler::PATH_TO_VIDEO_UPLOADS}; ?></code>. Click any video to edit its properties or thumbnails.</p>
+
 <div class="span-24 last" id="album_list">
+	<ul>
 	<?php foreach (${org_tubepress_uploads_admin_AdminPageHandler::ADMIN_ALBUM_ARRAY} as $album) : ?>
 	
-        <li id="<?php echo md5($album->getRelativeContainerPath()); ?>"><a><?php echo $album->getRelativeContainerPath(); ?></a>
-        <?php
-            $idArray[] =  md5($album->getRelativeContainerPath());
+        	<li id="video_<?php echo md5($album->getRelativeContainerPath()); ?>"><a><?php echo $album->getRelativeContainerPath(); ?></a>
+        	<?php
+            		$idArray[] =  'video_' . md5($album->getRelativeContainerPath());
         
-            if (sizeof($album->getRelativeVideoPaths()) === 0) {
-                echo '</li>';
-                continue;
-            }
-            ?>
-            <ul>
-            <?php foreach ($album->getRelativeVideoPaths() as $video) : ?>
-                <li rel="video" class='jstree-closed' id="<?php echo urlencode($video); ?>"><a><?php echo basename($video); ?></a></li>
-            <?php endforeach; ?>
-            </ul>
+            		if (sizeof($album->getRelativeVideoPaths()) === 0) {
+                		echo '</li>';
+                		continue;
+            		}
+            	?>
+            		<ul>
+            			<?php foreach ($album->getRelativeVideoPaths() as $video) : ?>
+                			<li rel="video" title="<?php echo base64_encode($video); ?>"><a><?php echo basename($video); ?></a></li>
+            			<?php endforeach; ?>
+            		</ul>
 	       </li>
 	<?php endforeach; ?>
+	</ul>
 </div>
 
 <script type="text/javascript">
-$.jstree._themes = "jstree/themes/";
-    jQuery("#album_list").jstree({
-    	"themes" : {
-    	             "theme" : "classic"
-    	          },
-    	"core" : { "animation" : 100,
-    	        	"initially_open" : [ "#<?php echo implode('", "#', $idArray); ?>" ]
-      	          	 },
-    	"types" : {
-            "types" : {
-                "video" : {
-                    "icon" : { 
-                        "image" : "famfam/film.png" //thank you! http://www.famfamfam.com/lab/icons/silk/
-                    },
-                }      
-            }
-        },
-        "html_data" : {
-             "data" : $("#album_list").html(),
-             "ajax" : { "url" : "<?php echo $_SERVER['PHP_SELF']; ?>",
-        	       "data" : function (n) {
-        	                    return { video : n.attr ? n.attr("id") : 0 };
-        	                }
-        	                  }
-         },
-        	    	
-    	"plugins" : [ "themes", "html_data", "checkbox", "ui", "cookies", "types" ]
-        }
 
-    );
+function toggleVideoEditor(base64encoded_relative_path) {
+	alert(base64encoded_relative_path);
+}
 
+jQuery(document).ready(function () {
+	jQuery("li[rel='video']").click(function () { toggleVideoEditor(jQuery(this).attr('title')); });
+});
+
+jQuery.jstree._themes = "jstree/themes/";
+
+jQuery("#album_list").jstree({
+    	"themes" 	: { 
+				"theme" : "classic" 
+			},
+    	"core" 		: {
+				"animation" : 100,
+    	        		"initially_open" : [ "#<?php echo implode('", "#', $idArray); ?>" ]
+      	         	},
+    	"types" 	: {
+            			"types" : {
+                			"video" : {
+                    				"icon" : { 
+                        				"image" : "famfam/film.png" //thank you! http://www.famfamfam.com/lab/icons/silk/
+                    				}
+                			}      
+            			}
+        		},
+        "html_data" 	: {
+             			"data" : $("#album_list").html()
+         		},
+     	"plugins" 	: [ "themes", "html_data", "ui", "cookies", "types" ]
+});
 </script>
