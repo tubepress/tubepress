fba5463baefc1b944f35177cb8b620d48eee546a
Eric Hough <houghdev@gmail.com>
2010-08-18 17:35:46 +0000

    Pagination service unit test work
    git-svn-id: http://tubepress.googlecode.com/svn/trunk@1117 4dae44e4-133b-0410-9315-df2bff685ba4

--- test/unit/org/tubepress/pagination/DiggStylePaginationServiceTest.php
+++ test/unit/org/tubepress/pagination/DiggStylePaginationServiceTest.php
@@ -8,49 +8,89 @@ class org_tubepress_pagination_DiggStylePaginationServiceTest extends TubePressU
 
     private $_prefix = '<div class="pagination"><span class="disabled">prev</span><span class="current">1</span><a rel=';
     
+    private static $_currentPage = 1;
+
     public function setup()
 	{
 		$this->initFakeIoc();
 	}
 
+	public function getMock($className)
+	{
+		$mock = parent::getMock($className);
+		switch ($className) {
+			case 'org_tubepress_querystring_QueryStringService':
+				$mock->expects($this->any())
+				     ->method('getFullUrl')
+				     ->will($this->returnValue('http://ehough.com'));
+				$mock->expects($this->any())
+				    ->method('getPageNum')
+				    ->will($this->returnValue(self::$_currentPage));
+				break;
+		}
+		return $mock;
+	}
+
     public function testAjax()
-    {
-        global $_SERVER;
-        $_SERVER['SERVER_PORT'] = 80;
-        $_SERVER['SERVER_NAME'] = 'ehough.com';
-        $_SERVER['REQUEST_URI'] = '';
-        
+    {       
         $this->setOptions(array(
             org_tubepress_options_category_Display::AJAX_PAGINATION => true,
             org_tubepress_options_category_Display::RESULTS_PER_PAGE => 4
         ));
         
-        $this->_tester('digg_test_callback_with_ajax',<<<EOT
+        $this->_tester(<<<EOT
 $this->_prefix"page=2">2</a><a rel="page=3">3</a><a rel="page=4">4</a><a rel="page=5">5</a>... <a rel="page=24">24</a><a rel="page=25">25</a><a rel="page=2">next</a></div>
 
 EOT
 );
     }
     
-    public function testNoAjax()
+    public function testNoAjaxHighPage()
     {
-        global $_SERVER;
-        $_SERVER['SERVER_PORT'] = 80;
-        $_SERVER['SERVER_NAME'] = 'ehough.com';
-        $_SERVER['REQUEST_URI'] = '';
-        
         $this->setOptions(array(
             org_tubepress_options_category_Display::RESULTS_PER_PAGE => 4
         ));
+
+	self::$_currentPage = 25;
         
-        $this->_tester('digg_test_callback_without_ajax',<<<EOT
+        $this->_tester(<<<EOT
+<div class="pagination"><a rel="nofollow" href="http://ehough.com?tubepress_page=24">prev</a><a rel="nofollow" href="http://ehough.com?tubepress_page=1">1</a><a rel="nofollow" href="http://ehough.com?tubepress_page=2">2</a>... <a rel="nofollow" href="http://ehough.com?tubepress_page=21">21</a> <a rel="nofollow" href="http://ehough.com?tubepress_page=22">22</a> <a rel="nofollow" href="http://ehough.com?tubepress_page=23">23</a> <a rel="nofollow" href="http://ehough.com?tubepress_page=24">24</a><span class="current">25</span><span class="disabled">next</span></div>
+
+EOT
+);
+    }
+
+    public function testNoAjaxMiddlePage()
+    {
+        $this->setOptions(array(
+            org_tubepress_options_category_Display::RESULTS_PER_PAGE => 4
+        ));
+
+	self::$_currentPage = 12;
+        
+        $this->_tester(<<<EOT
+<div class="pagination"><a rel="nofollow" href="http://ehough.com?tubepress_page=11">prev</a><a rel="nofollow" href="http://ehough.com?tubepress_page=1">1</a><a rel="nofollow" href="http://ehough.com?tubepress_page=2">2</a>... <a rel="nofollow" href="http://ehough.com?tubepress_page=11">11</a><span class="current">12</span> <a rel="nofollow" href="http://ehough.com?tubepress_page=13">13</a>... <a rel="nofollow" href="http://ehough.com?tubepress_page=24">24</a> <a rel="nofollow" href="http://ehough.com?tubepress_page=25">25</a><a rel="nofollow" href="http://ehough.com?tubepress_page=13">next</a></div>
+
+EOT
+);
+    }
+
+    public function testNoAjax()
+    {
+        $this->setOptions(array(
+            org_tubepress_options_category_Display::RESULTS_PER_PAGE => 4
+        ));
+
+	self::$_currentPage = 1;
+        
+        $this->_tester(<<<EOT
 $this->_prefix"nofollow" href="http://ehough.com?tubepress_page=2">2</a><a rel="nofollow" href="http://ehough.com?tubepress_page=3">3</a><a rel="nofollow" href="http://ehough.com?tubepress_page=4">4</a><a rel="nofollow" href="http://ehough.com?tubepress_page=5">5</a>... <a rel="nofollow" href="http://ehough.com?tubepress_page=24">24</a><a rel="nofollow" href="http://ehough.com?tubepress_page=25">25</a><a rel="nofollow" href="http://ehough.com?tubepress_page=2">next</a></div>
 
 EOT
 );
     }
     
-	private function _tester($callback, $expected)
+	private function _tester($expected)
 	{
 		$sut = new org_tubepress_pagination_DiggStylePaginationService();
 		$this->assertEquals($expected, $sut->getHtml(100));
