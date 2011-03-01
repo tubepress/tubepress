17a27587675dd7ca1d5a8f4552a2d169753f1a6f
Eric Hough <houghdev@gmail.com>
2011-02-28 20:26:01 -0800

    Initial move to "sys" directory

--- tubepress.php
+++ tubepress.php
@@ -4,7 +4,7 @@ Plugin Name: TubePress
 Plugin URI: http://tubepress.org
 Description: Displays gorgeous YouTube and Vimeo galleries in your posts, pages, and/or sidebar. Upgrade to <a href="http://tubepress.org/download/">TubePress Pro</a> for more features!
 Author: Eric D. Hough
-Version: 2.1.2
+Version: 2.2.0
 Author URI: http://ehough.com
 
 Copyright 2006 - 2011 Eric D. Hough (http://ehough.com)
@@ -28,7 +28,7 @@ along with TubePress.  If not, see <http://www.gnu.org/licenses/>.
 if (version_compare(PHP_VERSION, '5.0.2', '>=')) {
 
     function_exists('tubepress_load_classes')
-        || require dirname(__FILE__) . '/classes/tubepress_classloader.php';
+        || require dirname(__FILE__) . '/sys/classes/tubepress_classloader.php';
     tubepress_load_classes(array('org_tubepress_impl_ioc_IocContainer'));
     
     org_tubepress_impl_ioc_IocContainer::getInstance()->get('org_tubepress_api_bootstrap_Bootstrapper')->boot();
