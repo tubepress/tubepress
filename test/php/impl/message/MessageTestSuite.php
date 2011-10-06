491db776a4f2f6cf982c33f10d68bd309741d4a1
Eric Hough <eric@tubepress.org>
2011-10-05 18:39:56 -0700

    Fixing up WordPress message service

--- test/php/impl/message/MessageTestSuite.php
+++ test/php/impl/message/MessageTestSuite.php
@@ -1,5 +1,5 @@
 <?php
-require_once BASE . '/test/includes/TubePressUnitTest.php';
+require_once dirname(__FILE__) . '/../../../../test/includes/TubePressUnitTest.php';
 require_once 'WordPressMessageServiceTest.php';
 
 class org_tubepress_impl_message_MessageTestSuite
