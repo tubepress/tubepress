af3bb4e97ced2c92d82a859b408f28f0f9929e9e
Eric Hough <eric@tubepress.org>
2011-10-14 09:24:57 -0700

    Fixing tests for ChaingGang

--- test/php/impl/patterns/PatternsTestSuite.php
+++ test/php/impl/patterns/PatternsTestSuite.php
@@ -1,5 +1,5 @@
 <?php
-require_once BASE . '/test/includes/TubePressUnitTest.php';
+require_once dirname(__FILE__) . '/../../../includes/TubePressUnitTest.php';
 require_once 'cor/ChainGangTest.php';
 
 class org_tubepress_impl_patterns_PatternsTestSuite
