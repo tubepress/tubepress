0a2efd59e975049678e23314e4ca8de2d5580b7f
Eric Hough <eric@tubepress.org>
2012-02-07 08:58:48 -0800

    Upgrading to JW FLV Player 5.9

--- sys/scripts/wordpress/loader.php
+++ sys/scripts/wordpress/loader.php
@@ -1,19 +1,19 @@
 <?php
 /**
  * Copyright 2006 - 2011 Eric D. Hough (http://ehough.com)
- * 
+ *
  * This file is part of TubePress (http://tubepress.org)
- * 
+ *
  * TubePress is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * TubePress is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with TubePress.  If not, see <http://www.gnu.org/licenses/>.
  *
@@ -28,8 +28,9 @@
 $ioc = org_tubepress_impl_ioc_IocContainer::getInstance();
 $env = $ioc->get(org_tubepress_api_environment_Detector::_);
 
-if ($env->isWordPress()) {
-    
+//if ($env->isWordPress()) {
+
     $fs = $ioc->get(org_tubepress_api_filesystem_Explorer::_);
-    include $fs->getTubePressBaseInstallationPath() . '/../../../wp-blog-header.php';
-}
+    //include '/Users/ehough/Sites/tubepress_testing_ground/wordpress/wp-blog-header.php';
+    include '/var/www/tubepress_testing_ground/wordpress/wp-blog-header.php';
+//}
