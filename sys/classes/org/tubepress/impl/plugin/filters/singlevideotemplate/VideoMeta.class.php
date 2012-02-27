f8d29f2f97a1059568354046009e3e6c81b368a5
Eric Hough <eric@tubepress.org>
2012-02-26 18:05:27 -0800

    Fixing broken single video template filter

--- sys/classes/org/tubepress/impl/plugin/filters/singlevideotemplate/VideoMeta.class.php
+++ sys/classes/org/tubepress/impl/plugin/filters/singlevideotemplate/VideoMeta.class.php
@@ -26,17 +26,20 @@ class org_tubepress_impl_plugin_filters_singlevideotemplate_VideoMeta
 {
     public function alter_singleVideoTemplate(org_tubepress_api_template_Template $template, org_tubepress_api_video_Video $video, $providerName)
     {
-        $ioc            = org_tubepress_impl_ioc_IocContainer::getInstance();
-        $context        = $ioc->get(org_tubepress_api_exec_ExecutionContext::_);
-        $messageService = $ioc->get(org_tubepress_api_message_MessageService::_);
-
-        $metaNames  = org_tubepress_impl_util_LangUtils::getDefinedConstants(org_tubepress_api_const_options_names_Meta::_);
-        $shouldShow = array();
-        $labels     = array();
+        $ioc                       = org_tubepress_impl_ioc_IocContainer::getInstance();
+        $context                   = $ioc->get(org_tubepress_api_exec_ExecutionContext::_);
+        $messageService            = $ioc->get(org_tubepress_api_message_MessageService::_);
+        $optionDescriptorReference = $ioc->get(org_tubepress_api_options_OptionDescriptorReference::_);
+        $metaNames                 = org_tubepress_impl_util_LangUtils::getDefinedConstants(org_tubepress_api_const_options_names_Meta::_);
+        $shouldShow                = array();
+        $labels                    = array();
 
         foreach ($metaNames as $metaName) {
+
+            $optionDescriptor = $optionDescriptorReference->findOneByName($metaName);
+
             $shouldShow[$metaName] = $context->get($metaName);
-            $labels[$metaName]     = $messageService->_('video-' . $metaName);
+            $labels[$metaName]     = $messageService->_($optionDescriptor->getLabel());
         }
 
         $template->setVariable(org_tubepress_api_const_template_Variable::META_SHOULD_SHOW, $shouldShow);
