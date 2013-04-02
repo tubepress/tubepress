8c0970ec87f81d64770da055f6486edcdaeae762
Eric Hough <eric@tubepress.org>
2013-04-02 19:02:35 -0400

    Removing stale link in comments for system templates.

--- src/main/resources/default-themes/youtube/gallery.tpl.php
+++ src/main/resources/default-themes/youtube/gallery.tpl.php
@@ -7,9 +7,6 @@
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- *
- * Uber simple/fast template for TubePress. Idea from here: http://seanhess.net/posts/simple_templating_system_in_php
- * Sure, maybe your templating system of choice looks prettier but I'll bet it's not faster :)
  */
 ?>
 
