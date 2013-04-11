2b00d5abaced0db759c6b977a9782cde02130d0f
Eric Hough <eric@tubepress.org>
2013-04-11 14:48:19 -0400

    Re-adding deprecated code

---
+++ src/main/php/deprecated/classes/org/tubepress/api/const/options/names/Meta.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Copyright 2006 - 2013 TubePress LLC (http://tubepress.org)
+ *
+ * This file is part of TubePress (http://tubepress.org)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/**
+ * These classes are here for backwards compatability only.
+ */
+class org_tubepress_api_const_options_names_Meta extends tubepress_api_const_options_names_Meta {}
