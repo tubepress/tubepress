b97ee2a06ea02744ad2fa0223feecaa4015a8c6a
Eric Hough <eric@tubepress.org>
2015-06-04 13:04:53 -0700

    Form functionality is mostly complete

--- src/add-ons/core/classes/tubepress/app/impl/options/ui/Form.php
+++ src/add-ons/core/classes/tubepress/app/impl/options/ui/Form.php
@@ -15,12 +15,7 @@
 class tubepress_app_impl_options_ui_Form implements tubepress_app_api_options_ui_FormInterface
 {
     /**
-     * @var tubepress_app_api_environment_EnvironmentInterface
-     */
-    private $_environment;
-
-    /**
-     * @var tubepress_app_api_options_PersistenceInterface
+     * @var tubepress_app_impl_options_Persistence
      */
     private $_persistence;
 
@@ -44,17 +39,50 @@ class tubepress_app_impl_options_ui_Form implements tubepress_app_api_options_ui
      */
     private $_cssAndJsGenerationHelper;
 
-    public function __construct(tubepress_lib_api_template_TemplatingInterface     $templating,
-                                tubepress_app_api_environment_EnvironmentInterface $environment,
-                                tubepress_app_api_options_PersistenceInterface     $persistence,
-                                tubepress_platform_api_util_StringUtilsInterface   $stringUtils,
-                                tubepress_app_impl_html_CssAndJsGenerationHelper   $cssAndJsGenerationHelper)
+    /**
+     * @var tubepress_lib_api_http_RequestParametersInterface
+     */
+    private $_httpRequestParams;
+
+    /**
+     * @var tubepress_platform_api_log_LoggerInterface
+     */
+    private $_logger;
+
+    /**
+     * @var bool
+     */
+    private $_shouldLog;
+
+    /**
+     * @var tubepress_app_api_options_PersistenceInterface[]
+     */
+    private $_multiSourcePersistenceServices;
+
+    /**
+     * @var tubepress_app_impl_options_MultiSourcePersistenceBackend[]
+     */
+    private $_multiSourcePersistenceBackends;
+
+    /**
+     * @var tubepress_app_api_options_ui_MultiSourceFieldInterface[]
+     */
+    private $_cachedGroupIds;
+
+    public function __construct(tubepress_platform_api_log_LoggerInterface        $logger,
+                                tubepress_lib_api_template_TemplatingInterface    $templating,
+                                tubepress_app_impl_options_Persistence            $persistence,
+                                tubepress_platform_api_util_StringUtilsInterface  $stringUtils,
+                                tubepress_app_impl_html_CssAndJsGenerationHelper  $cssAndJsGenerationHelper,
+                                tubepress_lib_api_http_RequestParametersInterface $httpRequestParams)
     {
+        $this->_logger                   = $logger;
         $this->_templating               = $templating;
-        $this->_environment              = $environment;
         $this->_persistence              = $persistence;
         $this->_stringUtils              = $stringUtils;
         $this->_cssAndJsGenerationHelper = $cssAndJsGenerationHelper;
+        $this->_httpRequestParams        = $httpRequestParams;
+        $this->_shouldLog                = $logger->isEnabled();
     }
 
     /**
@@ -65,21 +93,13 @@ class tubepress_app_impl_options_ui_Form implements tubepress_app_api_options_ui
      */
     public function getHTML(array $errors = array(), $justSubmitted = false)
     {
-        $fields                            = $this->_buildFieldsArray();
-        $categories                        = $this->_buildCategoriesArray();
-        $categoryIdToProviderIdToFieldsMap = $this->_buildCategoryIdToProviderIdToFieldsMap($categories);
-        $fieldProviders                    = $this->_buildFieldProviderArray();
-
+        $hasErrors         = count($errors) > 0;
+        $fields            = $this->_buildFieldsArray(false, $hasErrors);
         $templateVariables = array(
 
-            'categories'                           => $categories,
-            'categoryIdToProviderIdToFieldsMap'    => $categoryIdToProviderIdToFieldsMap,
-            'errors'                               => $errors,
-            'fields'                               => $fields,
-            'isPro'                                => $this->_environment->isPro(),
-            'justSubmitted'                        => $justSubmitted,
-            'fieldProviders'                       => $fieldProviders,
-            'tubePressBaseUrl'                     => $this->_environment->getBaseUrl()->toString(),
+            'errors'        => $errors,
+            'fields'        => $fields,
+            'justSubmitted' => $justSubmitted,
         );
 
         return $this->_templating->renderTemplate('options-ui/form', $templateVariables);
@@ -95,7 +115,7 @@ class tubepress_app_impl_options_ui_Form implements tubepress_app_api_options_ui
         /**
          * @var tubepress_app_api_options_ui_FieldInterface[] $fields
          */
-        $fields = $this->_buildFieldsArray();
+        $fields = $this->_buildFieldsArray(true);
         $errors = array();
 
         foreach ($fields as $field) {
@@ -108,10 +128,7 @@ class tubepress_app_impl_options_ui_Form implements tubepress_app_api_options_ui
             }
         }
 
-        /**
-         * Let's save!
-         */
-        $this->_persistence->flushSaveQueue();
+        $this->_flushPersistence();
 
         return $errors;
     }
@@ -168,28 +185,26 @@ class tubepress_app_impl_options_ui_Form implements tubepress_app_api_options_ui
         $this->_fieldProviders = $providers;
     }
 
-    private function _buildCategoriesArray()
-    {
-        $toReturn = array();
-
-        foreach ($this->_fieldProviders as $fieldProvider) {
-
-            $toReturn = array_merge($toReturn, $fieldProvider->getCategories());
-        }
-
-        return $toReturn;
-    }
-
-    private function _buildFieldsArray()
+    private function _buildFieldsArray($fromSubmission, $hasErrors = false)
     {
         $fields = array();
 
         foreach ($this->_fieldProviders as $fieldProvider) {
 
-            $fields = array_merge($fields, $fieldProvider->getFields());
+            $fieldsFromProvider = $fieldProvider->getFields();
+
+            foreach ($fieldsFromProvider as $fieldFromProvider) {
+
+                if (!($fieldFromProvider instanceof tubepress_app_api_options_ui_MultiSourceFieldInterface)) {
+
+                    $fields[] = $fieldFromProvider;
+                }
+            }
         }
 
-        $toReturn = array();
+        $multiSourceFields = $this->_buildMultiSourceFieldsArray($fromSubmission, $hasErrors);
+        $fields            = array_merge($fields, $multiSourceFields);
+        $toReturn          = array();
 
         /**
          * @var $fields tubepress_app_api_options_ui_FieldInterface[]
@@ -212,49 +227,197 @@ class tubepress_app_impl_options_ui_Form implements tubepress_app_api_options_ui
         return $toReturn;
     }
 
+    private function _buildMultiSourceFieldsArray($fromSubmit, $hasErrors)
+    {
+        if ($fromSubmit) {
+
+            return $this->_buildMultiSourceFieldsFromSubmission();
+        }
+
+        return $this->_buildMultiSourceFieldsFromStorage($hasErrors);
+    }
+
+    private function _buildMultiSourceFieldsFromSubmission()
+    {
+        $this->_collectGroupIdsFromSubmission();
+
+        $fieldClones                           = array();
+        $this->_multiSourcePersistenceBackends = array();
+        $this->_multiSourcePersistenceServices = array();
+        $persistedOptions                      = $this->_persistence->fetchAll();
+        $multiSourceFields                     = $this->_getMultiSourceFieldsFromProviders();
+
+        foreach ($this->_cachedGroupIds as $sourceGroupId) {
+
+            $backend                                 = new tubepress_app_impl_options_MultiSourcePersistenceBackend($persistedOptions);
+            $multiSourcePersistence                  = $this->_persistence->getCloneWithCustomBackend($backend);
+            $this->_multiSourcePersistenceBackends[] = $backend;
+            $this->_multiSourcePersistenceServices[] = $multiSourcePersistence;
+
+            /**
+             * @var $multiSourceFields tubepress_app_api_options_ui_MultiSourceFieldInterface[]
+             */
+            foreach ($multiSourceFields as $multiSourceField) {
+
+                $fieldClones[] = $multiSourceField->cloneForMultiSource("tubepress-multisource-$sourceGroupId-", $multiSourcePersistence);
+            }
+        }
+
+        $toReturn = array();
+
+        /**
+         * @var $fieldClones tubepress_app_api_options_ui_FieldInterface[]
+         */
+        foreach ($fieldClones as $collectedField) {
+
+            $toReturn[$collectedField->getId()] = $collectedField;
+        }
+
+        return $toReturn;
+    }
+
+    private function _collectGroupIdsFromSubmission()
+    {
+        $allParams             = $this->_httpRequestParams->getAllParams();
+        $this->_cachedGroupIds = array();
+
+        foreach ($allParams as $key => $value) {
+
+            if (preg_match_all('/^tubepress-multisource-([0-9]+)-.+$/', $key, $matches) !== 1) {
+
+                continue;
+            }
+
+            if (!is_array($matches) || count($matches) !== 2 || !is_array($matches[1]) || count($matches[1]) !== 1) {
+
+                continue;
+            }
+
+            $this->_cachedGroupIds[] = $matches[1][0];
+        }
+
+        $this->_cachedGroupIds = array_unique($this->_cachedGroupIds);
+
+        if ($this->_shouldLog) {
+
+            $this->_logger->debug(sprintf('Found %d multisource group(s): %s', count($this->_cachedGroupIds), json_encode($this->_cachedGroupIds)));
+        }
+    }
+
+    private function _buildMultiSourceFieldsFromStorage($hasErrors)
+    {
+        $allPersistedOptions = $this->_persistence->fetchAll();
+
+        $toReturn = $this->_buildMultiSourceFieldsFromStoredSources($allPersistedOptions, $hasErrors);
+
+        if (!$toReturn) {
+
+            $toReturn = $this->_buildMultiSourceFieldsFromLegacyStorage($allPersistedOptions, $hasErrors);
+        }
+
+        $this->_cachedGroupIds = array();
+
+        return $toReturn;
+    }
+
+    private function _buildMultiSourceFieldsFromStoredSources(array $allPersistedOptions, $hasErrors)
+    {
+        if (!isset($allPersistedOptions[tubepress_app_api_options_Names::SOURCES])) {
+
+            return null;
+        }
+
+        $storedSources  = $allPersistedOptions[tubepress_app_api_options_Names::SOURCES];
+        $decodedSources = json_decode($storedSources, true);
+
+        if (!$decodedSources) {
+
+            return null;
+        }
+
+        $toReturn = array();
+
+        for ($index = 0; $index < count($decodedSources); $index++) {
+
+            $decodedSource = $decodedSources[$index];
+            $mergedOptions = array_merge($allPersistedOptions, $decodedSource);
+            $fields        = $this->_buildFieldArrayForSingleSource($mergedOptions, $hasErrors, $index);
+            $toReturn      = array_merge($toReturn, $fields);
+        }
+
+        return $toReturn;
+    }
+
+    private function _buildMultiSourceFieldsFromLegacyStorage(array $allPersistedOptions, $hasErrors)
+    {
+        return $this->_buildFieldArrayForSingleSource($allPersistedOptions, $hasErrors, 0);
+    }
+
+    private function _buildFieldArrayForSingleSource(array $options, $hasErrors, $index)
+    {
+        if ($hasErrors && isset($this->_cachedGroupIds[$index])) {
+
+            $prefix = $this->_cachedGroupIds[$index];
+
+        } else {
+
+            $prefix = 'tubepress-multisource-' . mt_rand(100000, 1000000) . '-';
+        }
+
+        $multiSourcePersistenceBackend = new tubepress_app_impl_options_MultiSourcePersistenceBackend($options);
+        $readOnlyPersistence           = $this->_persistence->getCloneWithCustomBackend($multiSourcePersistenceBackend);
+        $toReturn                      = array();
+        $multiSourceFields             = $this->_getMultiSourceFieldsFromProviders();
+
+        foreach ($multiSourceFields as $multiSourceField) {
+
+            $toReturn[] = $multiSourceField->cloneForMultiSource($prefix, $readOnlyPersistence);
+        }
+
+        return $toReturn;
+    }
+
     /**
-     * @param tubepress_app_api_options_ui_ElementInterface[] $categories
-     *
-     * @return array
+     * @return tubepress_app_api_options_ui_MultiSourceFieldInterface[]
      */
-    private function _buildCategoryIdToProviderIdToFieldsMap(array $categories)
+    private function _getMultiSourceFieldsFromProviders()
     {
         $toReturn = array();
 
-        foreach ($categories as $category) {
+        foreach ($this->_fieldProviders as $fieldProvider) {
 
-            $categoryId = $category->getId();
+            $fieldsFromProvider = $fieldProvider->getFields();
 
-            if (!isset($toReturn[$categoryId])) {
+            foreach ($fieldsFromProvider as $fieldFromProvider) {
 
-                $toReturn[$categoryId] = array();
-            }
+                if ($fieldFromProvider instanceof tubepress_app_api_options_ui_MultiSourceFieldInterface) {
 
-            foreach ($this->_fieldProviders as $fieldProvider) {
-
-                $map = $fieldProvider->getCategoryIdsToFieldIdsMap();
-
-                if (!isset($map[$categoryId])) {
-
-                    continue;
+                    $toReturn[] = $fieldFromProvider;
                 }
-
-                $toReturn[$categoryId][$fieldProvider->getId()] = $map[$categoryId];
             }
         }
 
         return $toReturn;
     }
 
-    private function _buildFieldProviderArray()
+    private function _flushPersistence()
     {
-        $toReturn = array();
+        foreach ($this->_multiSourcePersistenceServices as $multiSourcePersistenceService) {
 
-        foreach ($this->_fieldProviders as $fieldProvider) {
-
-            $toReturn[$fieldProvider->getId()] = $fieldProvider;
+            $multiSourcePersistenceService->flushSaveQueue();
         }
 
-        return $toReturn;
+        $sources = array();
+
+        foreach ($this->_multiSourcePersistenceBackends as $multiSourcePersistenceBackend) {
+
+            $sources[] = $multiSourcePersistenceBackend->getPersistenceQueue();
+        }
+
+        $sources = json_encode($sources);
+
+        $this->_persistence->queueForSave(tubepress_app_api_options_Names::SOURCES, $sources);
+
+        $this->_persistence->flushSaveQueue();
     }
 }
