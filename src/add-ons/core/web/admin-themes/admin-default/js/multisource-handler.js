13768eaeb2000960a539997b8ad76de7a82f730c
Eric Hough <eric@tubepress.org>
2015-06-27 22:08:22 -0700

    More multisource UI work

--- src/add-ons/core/web/admin-themes/admin-default/js/multisource-handler.js
+++ src/add-ons/core/web/admin-themes/admin-default/js/multisource-handler.js
@@ -13,12 +13,12 @@
 
         var getSourceContainerForElement = function (element) {
 
-                return element.closest('.js-multisource-single-source-outermost');
+                return element.closest('.js-multisource-single-container');
             },
 
             getAllSourceContainers = function () {
 
-                return jquery('.js-multisource-single-source-outermost');
+                return jquery('.js-multisource-single-container');
             };
 
         return {
@@ -26,79 +26,6 @@
             getSourceContainerForElement : getSourceContainerForElement,
             getAllSourceContainers       : getAllSourceContainers
         }
-    }());
-
-    /**
-     * Add source button
-     */
-    (function () {
-
-        var newRandomSourceId = function () {
-
-                return Math.floor(Math.random() * (1000000 - 100000) + 100000);
-            },
-
-            replaceIds = function (element, oldId, newId) {
-
-                var attrs = [ 'id', 'href', 'data-multisourceid', 'aria-labelledby', 'aria-controls'],
-                    attribute,
-                    oldAttrValue,
-                    newAttrValue,
-                    index;
-
-                for (index = 0; index < attrs.length; index += 1) {
-
-                    attribute    = attrs[index];
-                    oldAttrValue = element.attr(attribute);
-
-                    if (oldAttrValue) {
-
-                        newAttrValue = oldAttrValue.replace(oldId, newId);
-                        element.attr(attribute, newAttrValue);
-                    }
-                }
-
-                if (element.data('multisourceid') !== undefined) {
-
-                    element.data('multisourceid', newId.toString());
-                }
-            },
-
-            onAddSourceButtonClicked = function () {
-
-                win.tubePressBeacon.publish('newsource.start');
-
-                var last      = sourceFinder.getAllSourceContainers().last(),
-                    oldId     = last.data('multisourceid'),
-                    newId     = newRandomSourceId(),
-                    clone     = last.clone(false).find('*').andSelf().each(function () {
-
-                        replaceIds(jquery(this), oldId, newId);
-
-                    }).first(),
-                    appendTo  = last.parent(),
-                    beacon    = win.tubePressBeacon,
-                    eventData = {
-
-                        'oldId'        : oldId,
-                        'newId'        : newId,
-                        'originSource' : last,
-                        'newSource'    : clone
-                    };
-
-                beacon.publish('newsource.pre', eventData);
-
-                clone.appendTo(appendTo);
-
-                beacon.publish('newsource.post', eventData);
-            },
-
-            init = function () {
-
-                jquery('#js-multisource-button-add').click(onAddSourceButtonClicked);
-            };
-
-        jquery(init);
     }());
 
     /**
@@ -184,7 +111,7 @@
     }());
 
     /**
-     * Accordion handling.
+     * Expands the last/single source.
      */
     (function () {
 
@@ -193,24 +120,121 @@
                 var colls = jquery('#gallery_source_category .collapse'),
                     last  = colls.last();
 
-                last.collapse('show');
+                //last.collapse('show');
             },
 
             init = function () {
 
                 win.tubePressBeacon.subscribe('newsource.post', onAfterNewSourceAdded);
-
-                if (sourceFinder.getAllSourceContainers().length === 1) {
-
-                    onAfterNewSourceAdded();
-                }
             };
 
         jquery(init);
     }());
 
     /**
-     * Title bar handling.
+     * Add source button
+     */
+    (function () {
+
+        var newRandomSourceId = function () {
+
+                return Math.floor(Math.random() * (1000000 - 100000) + 100000);
+            },
+
+            replaceIds = function (element, oldId, newId) {
+
+                var attrs = [ 'id', 'href', 'data-multisourceid', 'aria-labelledby', 'aria-controls'],
+                    attribute,
+                    oldAttrValue,
+                    newAttrValue,
+                    index;
+
+                for (index = 0; index < attrs.length; index += 1) {
+
+                    attribute    = attrs[index];
+                    oldAttrValue = element.attr(attribute);
+
+                    if (oldAttrValue) {
+
+                        newAttrValue = oldAttrValue.replace(oldId, newId);
+                        element.attr(attribute, newAttrValue);
+                    }
+                }
+
+                if (element.data('multisourceid') !== undefined) {
+
+                    element.data('multisourceid', newId.toString());
+                }
+            },
+
+            onAddSourceButtonClicked = function () {
+
+                win.tubePressBeacon.publish('newsource.start');
+
+                var allContainers = sourceFinder.getAllSourceContainers(),
+                    last          = allContainers.last(),
+                    oldId         = last.data('multisourceid'),
+                    newId         = newRandomSourceId(),
+                    clone         = last.clone(false).find('*').andSelf().each(function () {
+
+                        replaceIds(jquery(this), oldId, newId);
+
+                    }).first(),
+                    appendTo  = last.parent(),
+                    beacon    = win.tubePressBeacon,
+                    eventData = {
+
+                        'oldId'        : oldId,
+                        'newId'        : newId,
+                        'originSource' : last,
+                        'newSource'    : clone
+                    },
+
+                    whenCollapseDoneCallback = function () {
+
+                        clone.find('.collapse.in').removeClass('in');
+
+                        clone.css('background-color', '#FFFF80');
+                        clone.appendTo(appendTo);
+
+                        clone.animate({
+                            backgroundColor: 'white'
+                        }, 1700);
+
+                        beacon.publish('newsource.post', eventData);
+                    },
+
+                    allDoneCollapsing = function () {
+
+                        var allDone = jquery('.js-multisource-single-container .collapse.in,.js-multisource-single-container .collapsing').length === 0;
+
+                        if (allDone) {
+
+                            whenCollapseDoneCallback();
+
+                        } else {
+
+                            setTimeout(allDoneCollapsing, 100);
+                        }
+                    };
+
+                beacon.publish('newsource.pre', eventData);
+
+                allContainers.find('.collapse').collapse('hide');
+
+                setTimeout(allDoneCollapsing, 100);
+            },
+
+            init = function () {
+
+                jquery('#js-multisource-button-add').click(onAddSourceButtonClicked);
+            };
+
+        jquery(init);
+    }());
+
+    /**
+     * Title bar text and icon handling.
      */
     (function () {
 
@@ -224,30 +248,50 @@
     }());
 
     /**
-     * Reorder handler.
+     * Initializes sorting.
      */
     (function () {
 
-        var getAccordion = function () {
-
-                return jquery('#js-multisource-accordion');
-
-            },
-
-            onSortStop = function () {
+        var onSortStop = function () {
 
                 win.tubePressBeacon.publish('sources.sort');
             },
 
             init = function () {
 
-                /**
-                 * Make the accordion sortable.
-                 */
-                getAccordion().sortable({
+                jquery('#gallery_source_category > .row > .col-xs-12').sortable({
 
                     stop: onSortStop
                 });
+            };
+
+        jquery(init);
+
+    }());
+
+    /**
+     * Collapses and expands content when title bar is clicked.
+     */
+    (function () {
+
+        var onTitleBarClicked = function (e) {
+
+                var clicked         = jquery(e.target),
+                    closestTitleBar = clicked.closest('.multisource-title-bar'),
+                    tabArea         = closestTitleBar.next('.multisource-tab-area'),
+                    isDelete        = clicked.hasClass('js-gallery-sources-delete-initiate'),
+                    isSort          = !isDelete && clicked.hasClass('multisource-control-reorder');
+
+                if (!isDelete && !isSort) {
+
+                    jquery('.multisource-tab-area').not(tabArea).collapse('hide');
+                    tabArea.collapse('toggle');
+                }
+            },
+
+            init = function () {
+
+                jquery(document).on('click', '.multisource-title-bar', {}, onTitleBarClicked);
             };
 
         jquery(init);
