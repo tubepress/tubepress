6c818d0296434e125f56377174301f99c013aebf
Eric Hough <eric@tubepress.com>
2016-03-15 16:20:19 -0700

    Minor build improvements. Retaining comments in compress JS/CSS

--- src/add-ons/html/web/js/tubepress.js
+++ src/add-ons/html/web/js/tubepress.js
@@ -1,4 +1,6 @@
-/*!
+/**
+ * @license
+ *
  * Copyright 2006 - 2016 TubePress LLC (http://tubepress.com).
  * This file is part of TubePress (http://tubepress.com).
  * This Source Code Form is subject to the terms of the Mozilla Public
