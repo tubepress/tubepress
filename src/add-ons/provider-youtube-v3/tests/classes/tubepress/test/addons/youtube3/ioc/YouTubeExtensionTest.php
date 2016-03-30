faaf36992c0237a43c318c98a930e9a3f7b30fdb
Eric Hough <eric@tubepress.com>
2016-03-30 13:47:47 -0700

    Improving validation of YouTube source values

--- src/add-ons/provider-youtube-v3/tests/classes/tubepress/test/addons/youtube3/ioc/YouTubeExtensionTest.php
+++ src/add-ons/provider-youtube-v3/tests/classes/tubepress/test/addons/youtube3/ioc/YouTubeExtensionTest.php
@@ -121,11 +121,6 @@ class tubepress_test_youtube3_ioc_YouTubeExtensionTest extends tubepress_api_tes
         }
 
         $validators = array(
-            tubepress_api_options_listeners_RegexValidatingListener::TYPE_ONE_OR_MORE_WORDCHARS_OR_HYPHEN => array(
-                tubepress_youtube3_api_Constants::OPTION_YOUTUBE_PLAYLIST_VALUE,
-                tubepress_youtube3_api_Constants::OPTION_YOUTUBE_FAVORITES_VALUE,
-                tubepress_youtube3_api_Constants::OPTION_YOUTUBE_USER_VALUE
-            ),
             tubepress_api_options_listeners_RegexValidatingListener::TYPE_STRING_YOUTUBE_VIDEO_ID => array(
                 tubepress_youtube3_api_Constants::OPTION_YOUTUBE_RELATED_VALUE
             )
@@ -148,9 +143,14 @@ class tubepress_test_youtube3_ioc_YouTubeExtensionTest extends tubepress_api_tes
             }
         }
 
+        $oneOrMoreWordCharGroups = '/^[\w-]+(?:\s+\+\s+[\w-]+)*$/';
+
         $validators = array(
 
-            tubepress_youtube3_api_Constants::OPTION_API_KEY => '/^[\w-]*$/'
+            tubepress_youtube3_api_Constants::OPTION_API_KEY                 => '/^[\w-]*$/',
+            tubepress_youtube3_api_Constants::OPTION_YOUTUBE_PLAYLIST_VALUE  => $oneOrMoreWordCharGroups,
+            tubepress_youtube3_api_Constants::OPTION_YOUTUBE_FAVORITES_VALUE => $oneOrMoreWordCharGroups,
+            tubepress_youtube3_api_Constants::OPTION_YOUTUBE_USER_VALUE      => $oneOrMoreWordCharGroups,
         );
 
         foreach ($validators as $optionName => $pattern) {
